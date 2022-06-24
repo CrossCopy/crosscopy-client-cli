e3592691701542ddac22efeaae36a51f91285c48
Huakun Shen <huakun.shen@huakunshen.com>
2022-06-24 14:00:16 -0400

    Get login command fixed partially. core package failing

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,7 +1,7 @@
-import {Command, Flags} from '@oclif/core'
-import {CryptoService} from '@crosscopy/core'
-import * as inquirer from 'inquirer'
-import gqlSchema from '@crosscopy/graphql-schema'
+import {Command, Flags} from '@oclif/core';
+import {CryptoService} from '@crosscopy/core';
+import * as inquirer from 'inquirer';
+import gqlSchema from '@crosscopy/graphql-schema';
 
 /**
  * Sample Command
@@ -9,31 +9,31 @@ import gqlSchema from '@crosscopy/graphql-schema'
  * ./bin/dev login -e user0@crosscopy.io -p password0
  */
 
-const {getSdk} = gqlSchema.requests
-import {GraphQLClient} from 'graphql-request'
+const {getSdk} = gqlSchema.requests;
+import {GraphQLClient} from 'graphql-request';
 
-const gqlClient = new GraphQLClient('https://api.crosscopy.io/graphql')
-const sdk = getSdk(gqlClient)
+const gqlClient = new GraphQLClient('https://api.crosscopy.io/graphql');
+const sdk = getSdk(gqlClient);
 
 export default class Login extends Command {
-  static description = 'Login to CrossCopy Cloud'
+  static description = 'Login to CrossCopy Cloud';
 
   static examples = [
     'xcopy login',
     'xcopy login -e username@email.com -p password',
-  ]
+  ];
 
   static flags = {
     email: Flags.string({char: 'e', description: 'Email'}),
     password: Flags.string({char: 'p', description: 'Password'}),
-  }
+  };
 
   public async run(): Promise<void> {
-    const {flags} = await this.parse(Login)
+    const {flags} = await this.parse(Login);
     // type Prompt = Parameters<typeof inquirer.prompt>
-    const prompts: {name: string; message: string; type: string}[] = []
+    const prompts: {name: string; message: string; type: string}[] = [];
     if (!flags.email) {
-      prompts.push({name: 'email', message: 'Enter your Email', type: 'input'})
+      prompts.push({name: 'email', message: 'Enter your Email', type: 'input'});
     }
 
     if (!flags.password) {
@@ -41,30 +41,30 @@ export default class Login extends Command {
         name: 'password',
         message: 'Enter your Password',
         type: 'password',
-      })
+      });
     }
 
     const responses: {email?: string; password?: string} =
-      await inquirer.prompt(prompts)
-    const email = flags.email || responses.email
-    const password = flags.password || responses.password
+      await inquirer.prompt(prompts);
+    const email = flags.email || responses.email;
+    const password = flags.password || responses.password;
     if (!email || !password)
-      throw new Error('Email and Password have to be defined')
-    const crypto = CryptoService.instance
-    crypto.init(password)
-    this.log(crypto.passwordHash)
-    // console.log(crypto.sha256('Message'))
+      throw new Error('Email and Password have to be defined');
+    // const crypto = CryptoService.instance;
+    // console.log(crypto);
+    // crypto.init(password);
+    // this.log(crypto.passwordHash);
     sdk
       .login({email, password})
       .then((res) => {
-        console.log(res.login?.accessToken)
-        console.log(res.login?.success)
+        console.log(res.login?.accessToken);
+        console.log(res.login?.success);
         if (res.login?.message) {
-          this.log(res.login?.message)
+          this.log(res.login?.message);
         }
       })
       .catch((error) => {
-        this.error(error)
-      })
+        this.error(error);
+      });
   }
 }
